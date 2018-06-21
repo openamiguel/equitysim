f329c7d1ef54b05b6e592156319b7f67a66b1d51
openamiguel <openamiguel@gmail.com>
2018-06-20 23:21:16 -0700

    return_calculator.py version 3.2.0
    changes Sharpe ratio formula to the "original" version

--- return_calculator.py
+++ return_calculator.py
@@ -1,6 +1,6 @@
 ## This code computes a few basic metrics of returns and performance. 
 ## Author: Miguel Opeña
-## Version: 3.1.1
+## Version: 3.2.0
 
 import numpy as np
 import pandas as pd
@@ -20,13 +20,13 @@ def overall_returns(prices):
 	"""
 	return 100 * (prices[-1] - prices[0]) / abs(prices[0])
 
-def sharpe_ratio(portfolio, risk_free_rate=0.0194):
+def sharpe_ratio(portfolio, risk_free_rate=1.94):
 	""" Calculates the Sharpe ratio of the given portfolio.
 		Inputs: portfolio dataframe, 
 	"""
 	price = portfolio.close.values.tolist()
 	returns = np.array(get_rolling_returns(price))
-	sharpe = (returns.mean() / 100 - risk_free_rate) / (returns.std() * np.sqrt(len(price)))
+	sharpe = (returns.mean() - risk_free_rate) / (returns.std())
 	return sharpe
 
 def portfolio_valuation(portfolio, baseline):
