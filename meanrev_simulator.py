f92eaf0511b1d613ece6b3d54bc33ff7b4fc4989
openamiguel <openamiguel@gmail.com>
2018-06-22 22:40:40 -0700

    meanrev_simulator.py version 1.9.0
    zscore_distance is much cleaner, but new version is buggy where the zscore dataframe is set up (Line 95)

--- meanrev_simulator.py
+++ meanrev_simulator.py
@@ -1,6 +1,6 @@
 ## This code models a very basic mean reversion strategy, using daily closing prices of one stock. 
 ## Author: Miguel Opeña
-## Version: 1.8.0
+## Version: 1.9.0
 
 import pandas as pd
 import numpy as np
@@ -62,49 +62,80 @@ def crossover(price_with_trends, startDate, endDate, startValue=1000, numTrades=
 			# Execute short positions
 			portfolio.price[date] = portfolio.price[previous_date] + numTrades * current_price
 			numPositions -= numTrades
-			print("Acquired {0} SHORT positions in asset.".format(numTrades))
+			print("Acquired {0} SHORT positions in asset.\n".format(numTrades))
 		# Otherwise, simply hold one's position
 		else: 
 			portfolio.price[date] = portfolio.price[previous_date]
 			print("No positional movement in asset held.\n")
 		previous_date = date
 		portfolioVal = portfolio.price[date]
+	# Return dataframe with timestamp, portfolio price over time
 	return portfolio
 
-def zscore_distance(priceRollingData, startingValue=1000, numBuys=1):
-	price = priceRollingData.close.values.tolist()
-	roll = priceRollingData.roll.values.tolist()
-	rollTrend = priceRollingData.rollTrend.values.tolist()
-	stdev = np.std(roll)
-	zscores = [(roll[i] - rollTrend[i]) / stdev for i in range(len(price))]
-
+def zscore_distance(price_with_trends, startDate, endDate, startValue=1000, numTrades=1):
+	"""	Simulates a zscore proximity strategy for a trend and baseline. 
+		Sells if trend crosses down below baseline, buys if trend crosses up above baseline, unloads if insufficient distance. 
+		Inputs: price data with trend and baseline, start date for price window, end date for price window,  
+			starting value of portfolio, the number of trades in each order
+		Outputs: dataframe of timestamp index and price
+	"""
+	# Saves timestamp to give the portfolio output an index
+	price_with_trends_window = price_with_trends[startDate:endDate]
+	timestamp = price_with_trends_window.index
+	previous_date = startDate
+	# Initialize portfolio with zero positions
 	numPositions = 0
-	portfolio = startingValue
-
-	for i in range(len(price)):
-		current_price = price[i]
-		thisZ = zscores[i]
-		if numBuys * current_price >= portfolio:
-			print("It seems you have run out of funds.")
+	portfolio = pd.DataFrame(startValue, index=timestamp, columns=['price'])
+	# Initialize portfolio value
+	portfolioVal = startValue
+	# Builds dataframe of z-scores over time
+	stdev = price_with_trends_window.trend.std()
+	zscores = pd.DataFrame(price_with_trends_window.trend)
+	zscores.columns = ['baseline']
+	zscores = zscores.subtract(price_with_trends_window.baseline, axis=1)
+	print(zscores)
+	zscores = zscores.divide(stdev)
+	zscores.columns = ['zscore']
+	# Iterates through every datte in the window
+	for date in timestamp:
+		print("The date is {0}. Current portfolio value at {1} with {2} positions held in asset.".format(date, portfolioVal, numPositions))
+		if date == startDate:
+			continue
+		# Gets asset values at current time
+		current_price = price_with_trends_window.price[date]
+		current_trend = price_with_trends_window.trend[date]
+		current_baseline = price_with_trends_window.baseline[date]
+		current_zscore = zscores.zscore[date]
+		# We don't want to run out of funds for trading. 
+		if numTrades * current_price >= portfolioVal:
+			print("You have insufficient funds to continue trading.")
 			break
 		# Sell short if z-score > 1
-		if thisZ > 1:
-			portfolio += current_price * numBuys
-			numPositions -= numBuys
-			print("Acquired %d SHORT positions.\n" % numBuys)
+		if current_zscore > 1:
+			portfolio.price[date] = portfolio.price[previous_date] + current_price * numTrades
+			numPositions -= numTrades
+			print("Acquired {0} SHORT positions in asset.\n".format(numTrades))
 		# Buy long if z-score < -1
-		elif thisZ < -1: 
-			portfolio -= current_price * numBuys
-			numPositions += numBuys
-			print("Acquired %d LONG positions.\n" % numBuys)
-		# Clear positions if z-score is too low
-		elif abs(thisZ) < 0.5:
-			portfolio += current_price * numPositions
+		elif current_zscore < -1: 
+			portfolio.price[date] = portfolio.price[previous_date] - current_price * numTrades
+			numPositions += numTrades
+			print("Acquired {0} LONG positions in asset.\n".format(numTrades))
+		# Clear positions if magnitude of z-score is too low
+		elif abs(current_zscore) < 0.5:
+			portfolio.price[date] = portfolio.price[previous_date] + current_price * numPositions
 			# Useless to display dumping 0 positions
 			if numPositions > 0:
-				print("Dumped %d positions. " % numPositions)
+				print("Cleared {0} positions in asset.\n".format(numPositions))
+			else:
+				print("No positions to clear.\n")
 			numPositions = 0
-
+		# In all other cases, hold positions
+		else: 
+			portfolio.price[date] = portfolio.price[previous_date]
+			print("No positional movement in asset.\n")
+		previous_date = date
+		portfolioVal = portfolio.price[date]
+	# Return dataframe with timestamp, portfolio price over time
 	return portfolio
 
 startDate = "2014-01-02"
@@ -121,6 +152,6 @@ price_with_trends = pd.concat([price, trend, baseline], axis=1)
 price_with_trends.columns = ['price','trend','baseline']
 price_with_trends.dropna(inplace=True)
 
-crossover(price_with_trends, startDate, endDate, startValue=start, numTrades=300)
+zscore_distance(price_with_trends, startDate, endDate, startValue=start, numTrades=300)
 
-plotter.price_plot(price_with_trends[startDate:endDate], "TSLA", folderPath=folderPath, names=['price', 'SMA01', 'SMA90'], showPlot=True)
+plotter.price_plot(price_with_trends[startDate:endDate], "GS", folderPath=folderPath, names=['price', 'SMA01', 'SMA90'], showPlot=True)
