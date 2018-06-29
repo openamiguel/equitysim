1a7ffff98fa9dbc9a4ef322a00f5a2f320550659
Eric Frankel <ericfrankel@Erics-MacBook-Pro.local>
2018-06-29 15:57:43 -0700

    setting up sampler class

--- price_predictor.py
+++ price_predictor.py
@@ -3,5 +3,24 @@
 ## Version: 1.0
 
 import numpy as np
-import pandas as pd
-import keras
+import pandas as pdb
+
+class Past_Sampler():
+    '''Gets training samples for predicting future data from past data'''
+
+    def __init__(self, N, K, sliding_window = True):
+        '''
+        Predict K future samples using N previous samples
+        '''
+        self.K = K
+        self.N = N
+        self.sliding_window = sliding_window
+
+    def transform(self, A):
+        M = self.N + self.K # Number of samples per prelim_download
+        #indexes
+        if self.sliding_window:
+            I = np.arange(M) + np.arange(A.shape[0] - M + 1).reshape(-1,2)
+        else:
+            if A.shape[0]%M == 0:
+                I = np.arange(M)+np.arange(0,A.shape[0],M).reshape(-1,1)
