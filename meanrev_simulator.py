0e587955dd5d684150c872ddce7994b69f19815c
openamiguel <openamiguel@gmail.com>
2018-06-29 19:02:14 -0700

    meanrev_simulator.py version 3.3.1
    calls the command_parser code to read command prompt options
    eliminates parameters of start and end date in the simulation functions
    tested and working

--- meanrev_simulator.py
+++ meanrev_simulator.py
@@ -1,26 +1,27 @@
 ## This code models two strategies: a crossover and a zscore distance. Both rely on mean reversion principles.
 ## Author: Miguel Opeña
-## Version: 3.2.7
+## Version: 3.3.1
 
 import pandas as pd
 import sys
 
+import command_parser
 import plotter
 import return_calculator
 import single_download
 import technicals_calculator
 
-def crossover(price_with_trends, start_date, end_date, startvalue=1000, numtrades=1, switch=False):
+def crossover(price_with_trends, startvalue=1000, numtrades=1, switch=False):
 	"""	Simulates a crossover strategy for a trend and baseline. 
 		Sells if trend crosses down below baseline, buys if trend crosses up above baseline. 
-		Inputs: price data with trend and baseline, start date for price window, end date for price window,  
-			starting value of portfolio, the number of trades in each order, command to switch buy/sell signals
+		Inputs: price data with trend and baseline, starting value of portfolio, the number of trades in each order, 
+			command to switch buy/sell signals
 		Outputs: dataframe of timestamp index and price, dates to go long, dates to go short
 	"""
 	# Saves timestamp to give the portfolio output an index
-	price_with_trends_window = price_with_trends[start_date:end_date]
-	timestamp = price_with_trends_window.index
-	previous_date = start_date
+	timestamp = price_with_trends.index
+	previous_date = timestamp[0]
+	start_date = timestamp[0]
 	# Iniitalize lists of buy long and sell short dates
 	long_dates = []
 	short_dates = []
@@ -28,7 +29,7 @@ def crossover(price_with_trends, start_date, end_date, startvalue=1000, numtrade
 	numPositions = 0
 	portfolio = pd.DataFrame(startvalue, index=timestamp, columns=['price'])
 	# Initialize boolean check variable
-	isGreater = (price_with_trends_window.trend[start_date] > price_with_trends_window.baseline[start_date])
+	isGreater = (price_with_trends.trend[start_date] > price_with_trends.baseline[start_date])
 	# Portfolio value
 	portfolio_value = startvalue
 	# Start iterating through the price, trend, and baseline data
@@ -37,9 +38,9 @@ def crossover(price_with_trends, start_date, end_date, startvalue=1000, numtrade
 		# Seeds the dataframe with start value
 		if date == start_date:
 			continue
-		current_price = price_with_trends_window.price[date]
-		current_trend = price_with_trends_window.trend[date]
-		current_baseline = price_with_trends_window.baseline[date]
+		current_price = price_with_trends.price[date]
+		current_trend = price_with_trends.trend[date]
+		current_baseline = price_with_trends.baseline[date]
 		# Establish the conditions for executing trades
 		condition1 = current_trend < current_baseline and isGreater
 		condition2 = current_trend > current_baseline and not isGreater
@@ -85,18 +86,17 @@ def crossover(price_with_trends, start_date, end_date, startvalue=1000, numtrade
 	# Return dataframe with timestamp, portfolio price over time
 	return portfolio, long_dates, short_dates
 
-def zscore_distance(price_with_trends, start_date, end_date, startvalue=1000, numtrades=1, switch=False, zscorevalues=[-1,0.5,1]):
+def zscore_distance(price_with_trends, startvalue=1000, numtrades=1, switch=False, zscorevalues=[-1,0.5,1]):
 	"""	Simulates a zscore proximity strategy for a trend and baseline. 
 		Sells if trend crosses down below baseline, buys if trend crosses up above baseline, unloads if insufficient distance. 
-		Inputs: price data with trend and baseline, start date for price window, end date for price window,  
-			starting value of portfolio, the number of trades in each order, command to switch buy/sell signals,
-			list of z-scores to use as buy, sell, and clear signals
+		Inputs: price data with trend and baseline, starting value of portfolio, the number of trades in each order, 
+			command to switch buy/sell signals, list of z-scores to use as buy, sell, and clear signals
 		Outputs: dataframe of timestamp index and price, dates to go long, dates to go short
 	"""
 	# Saves timestamp to give the portfolio output an index
-	price_with_trends_window = price_with_trends[start_date:end_date]
-	timestamp = price_with_trends_window.index
-	previous_date = start_date
+	timestamp = price_with_trends.index
+	previous_date = timestamp[0]
+	start_date = timestamp[0]
 	# Iniitalize lists of buy long and sell short dates
 	long_dates = []
 	short_dates = []
@@ -106,10 +106,10 @@ def zscore_distance(price_with_trends, start_date, end_date, startvalue=1000, nu
 	# Initialize portfolio value
 	portfolio_value = startvalue
 	# Builds dataframe of z-scores over time using zscore formula: trend minus baseline, all divided by stdev
-	stdev = price_with_trends_window.trend.std()
-	zscores = pd.DataFrame(price_with_trends_window.trend)
+	stdev = price_with_trends.trend.std()
+	zscores = pd.DataFrame(price_with_trends.trend)
 	zscores.columns = ['baseline']
-	zscores = zscores.subtract(price_with_trends_window.baseline, axis=0)
+	zscores = zscores.subtract(price_with_trends.baseline, axis=0)
 	zscores = zscores.divide(stdev)
 	zscores.columns = ['zscore']
 	# Iterates through every datte in the window
@@ -118,9 +118,9 @@ def zscore_distance(price_with_trends, start_date, end_date, startvalue=1000, nu
 		if date == start_date:
 			continue
 		# Gets asset values at current time
-		current_price = price_with_trends_window.price[date]
-		current_trend = price_with_trends_window.trend[date]
-		current_baseline = price_with_trends_window.baseline[date]
+		current_price = price_with_trends.price[date]
+		current_trend = price_with_trends.trend[date]
+		current_baseline = price_with_trends.baseline[date]
 		current_zscore = zscores.zscore[date]
 		# We don't want to run out of funds for trading. 
 		if numtrades * current_price >= portfolio_value:
@@ -176,76 +176,51 @@ def main():
 	"""
 	prompts = sys.argv
 	## Handles which symbol the user wants to download.
-	symbol = ""
-	if "-symbol" in prompts:
-		symbol = prompts[prompts.index("-symbol") + 1]
-	else:
-		raise ValueError("No symbol provided. Please try again.")
+	symbol = command_parser.get_generic_from_prompts(prompts, "-symbol")
 	## Handles where the user wants to download their files. 
 	# Default folder path is relevant to the author only. 
-	folder_path = "C:/Users/Miguel/Documents/EQUITIES/stockDaily"
-	if "-folderPath" not in prompts:
-		print("Warning: the program will use default file paths, which may not be compatible on your computer.")
-	else: 
-		folder_path = prompts[prompts.index("-folderPath") + 1]
+	folder_path = command_parser.get_generic_from_prompts(prompts, query="-folderPath", default="C:/Users/Miguel/Documents/EQUITIES/stockDaily", req=False)
+	## Handles collection of the start and end dates for trading
+	start_date = command_parser.get_generic_from_prompts(prompts, query="-startDate")
+	end_date = command_parser.get_generic_from_prompts(prompts, query="-endDate")
+	## Handles which index/asset should be the baseline 
+	baselineSymbol = command_parser.get_generic_from_prompts(prompts, query="-baseline", default="^GSPC", req=False)
+	## Handles the file name of plot
+	plotName = command_parser.get_generic_from_prompts(prompts, query="-plotName", default="STRATEGY_01", req=False)
+	## Handles the initial value of the portfolio
+	start_value = command_parser.get_generic_from_prompts(prompts, query="-startValue", default=1000000, req=False)
+	start_value = float(start_value)
+	## Handles how many shares for each trade
+	num_shares = command_parser.get_generic_from_prompts(prompts, query="-numShares", default=1, req=False)
+	num_shares = int(num_shares)
+	## Handles whether one wants to show the plot
+	showplt = "-showPlot" in prompts
+	## Handles whether one wants to switch buy and sell signals
+	to_switch = "-switch" in prompts
 	## Sets up the price data from local drive
 	tick_data = single_download.fetch_symbol_from_drive(symbol, folderPath=folder_path)
 	trend = technicals_calculator.exponential_moving_average(tick_data.close, num_periods=30)
 	baseline = technicals_calculator.exponential_moving_average(tick_data.close, num_periods=90)
 	cmo = technicals_calculator.chande_momentum_oscillator(tick_data.close, num_periods=14)
+	# Sets up baseline index/asset
+	portfolio_baseline = single_download.fetch_symbol_from_drive(baselineSymbol, function="DAILY", folderPath=folder_path)
+	portfolio_baseline = portfolio_baseline[start_date:end_date]
 	# Consolidates the price, trend, and baseline into one dataframe
 	price_with_trends = pd.concat([tick_data.close, trend, baseline, cmo], axis=1)
 	price_with_trends.columns = ['price','trend','baseline','CMO']
 	price_with_trends.dropna(inplace=True)
-	## Handles collection of the four dates
-	# Gets the start date for portfolio trading
-	start_date = ""
-	if "-startDate" not in prompts:
-		raise ValueError("No start date provided. Please try again.")
-	else:
-		start_date = prompts[prompts.index("-startDate") + 1]
-	# Gets the end date for portfolio trading
-	end_date = ""
-	if "-endDate" not in prompts:
-		raise ValueError("No end date provided. Please try again.")
-	else:
-		end_date = prompts[prompts.index("-endDate") + 1]
-	## Handles which index/asset should be the baseline 
-	baselineSymbol = "^GSPC"
-	if "-baseline" not in prompts:
-		print("Default baseline: S&P 500 index")
-	else:
-		baselineSymbol = prompts[prompts.index("-baseline") + 1]
-	# Sets up baseline index/asset
-	portfolio_baseline = single_download.fetch_symbol_from_drive(baselineSymbol, function="DAILY", folderPath=folder_path)
-	portfolio_baseline = portfolio_baseline[start_date:end_date]
-	## Handles whether one wants to show the plot
-	showplt = ("-showPlot" in prompts)
-	## Handles the file name of plot
-	plotName = "STRATEGY_01"
-	if "-plotName" in prompts: plotName = prompts[prompts.index("-plotName") + 1]
-	## Handles the initial value of the portfolio
-	start_value = 1000000
-	if "-startValue" in prompts:
-		start_value = float(prompts[prompts.index("-startValue") + 1])
-	else:
-		print("Default of ${0} will be used for portfolio start value.".format(start_value))
-	## Handles how many shares for each trade
-	num_shares = 1
-	if "-numShares" in prompts: num_shares = int(prompts[prompts.index("-numShares") + 1])
-	## Handles whether one wants to switch buy and sell signals
-	to_switch = "-switch" in prompts
+	price_with_trends = price_with_trends[start_date:end_date]
 	## Handles choice between crossover and zscore strategy
 	portfolio = None
 	long_dates = []
 	short_dates = []
 	title = ""
 	if "-crossover" in prompts: 
-		portfolio, long_dates, short_dates = crossover(price_with_trends, start_date, end_date, startvalue=start_value, switch=to_switch, numtrades=num_shares)
+		portfolio, long_dates, short_dates = crossover(price_with_trends, startvalue=start_value, switch=to_switch, numtrades=num_shares)
 		portfolio.columns=['close']
 		title = "PORTFOLIO STRATEGY CROSSOVER"
 	elif "-zscore" in prompts:
-		portfolio, long_dates, short_dates = zscore_distance(price_with_trends, start_date, end_date, startvalue=start_value, switch=to_switch, numtrades=num_shares, zscorevalues=[-0.5,0.25,0.5])
+		portfolio, long_dates, short_dates = zscore_distance(price_with_trends, startvalue=start_value, switch=to_switch, numtrades=num_shares, zscorevalues=[-0.5,0.25,0.5])
 		portfolio.columns=['close']
 		title = "PORTFOLIO STRATEGY ZSCORE"
 	else:
