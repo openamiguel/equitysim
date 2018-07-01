e6dda99aed9bdf9a7ae1189198685a3338a8c8c3
openamiguel <openamiguel@gmail.com>
2018-06-30 21:54:53 -0700

    portfolio.py version 1.2.4
    builds a complete main method
    completed comments on apply_trades function
    adds parameter to apply_trades: a factor (e.g. 10 percent) of how much to seed the portfolio with
    
    changed the initial number of positions from a rounding function to a floor function
    bugs squashed include:
    fixed a call to prices.all_prices before it had been created
    fixed error where main function deleted all data in the prices dataframe
    fixed logical error in apply_trades where portfolio would always run out of money

--- portfolio.py
+++ portfolio.py
@@ -1,9 +1,10 @@
 ## This code uses trading signals from 
 ## -1 corresponds to sell short, 0 to hold, 1 to buy long, and 'X' to clear all positions
 ## Author: Miguel Opeña
-## Version: 1.2.0
+## Version: 1.2.4
 
 import logging
+from math import floor
 import pandas as pd
 
 import single_download
@@ -13,36 +14,32 @@ import technicals_calculator
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-def apply_trades(prices, trades, injection=1000000, numtrades=1):
+def apply_trades(prices, trades, initialval=10000, seed=0.1, numtrades=1):
 	"""	Applies a set of trades to a set of assets to calculate portfolio value over time
-		Inputs: dataframe of prices
+		Inputs: dataframe of prices for multiple symbols, dataframe of trade signals, initial value to invest in 
 	"""
 	# Saves timestamp to give the portfolio output an index
 	timestamp = prices.index
-	# Saves starting portfolio value
-	startval = prices.all_prices[timestamp[0]]
 	# Merges all the price columns into a consolidated column
 	prices['all_prices'] = prices.sum(axis=1)
+	# Saves starting portfolio value
+	startval = prices.all_prices[timestamp[0]]
 	# Checks if start value can cover one share of each asset
 	# Note: one could instead assume fractional shares
-	if startval > injection:
+	if startval > initialval:
 		logger.error("Error: insufficient funds to start trading.")
 		return None
 	# Saves the number of positions total
-	# Starts with as many positions as one can fill with given injection
-	numpositions = int(round(injection / startval))
+	# Starts with as many positions as one can fill with given initial value
+	numpositions = int(floor(seed * initialval / startval))
 	# Sets up the portfolio as a dataframe
-	portfolio = pd.DataFrame(injection - startval * numpositions, index=timestamp, columns=['price'])
+	portfolio = pd.DataFrame(initialval - startval * numpositions, index=timestamp, columns=['price'])
 	# Saves the previous date
 	last_date = timestamp[0]
 	# Iterates through all the dates (starts on second given date)
 	for date in timestamp[1:]:
 		current_price = prices.all_prices[date]
 		current_trade = trades.all_trades[date]
-		# Checks if any assets left to trade
-		if numtrades * current_price > portfolio.price[last_date]:
-			logger.error("Error: Portfolio has run out of funds at {}.".format(date))
-			return None
 		# Buys positions, reducing portfolio value
 		if current_trade == 1:
 			portfolio.price[date] = portfolio.price[last_date] - current_price * numtrades
@@ -68,6 +65,10 @@ def apply_trades(prices, trades, injection=1000000, numtrades=1):
 		# No other values are currently permitted
 		else:
 			logger.error("Error: bad trading signal found at date {0}".format(date))
+		# Checks if any assets left to trade
+		if portfolio.price[last_date] <= 0:
+			logger.error("Error: Portfolio has run out of funds at {}.".format(date))
+			return None
 		logger.info('Date: {0}\tPortfolio value at ${1}.'.format(date, portfolio.price[date]))
 		last_date = date
 	return portfolio
@@ -78,15 +79,14 @@ def main():
 	start_date = "2014-01-01"
 	end_date = "2018-06-28"
 	tick_data = single_download.fetch_symbol_from_drive(symbol, folderPath=folder_path)
-	tick_data =tick_data[start_date:end_date]
+	tick_data = tick_data[start_date:end_date]
+	prices = pd.concat([tick_data.close], axis=1)
 	trend = technicals_calculator.exponential_moving_average(tick_data.close, num_periods=30)
 	baseline = technicals_calculator.exponential_moving_average(tick_data.close, num_periods=90)
 	trend_baseline = pd.concat([trend, baseline], axis=1)
 	trend_baseline.columns = ['trend','baseline']
-	trades = zscore_distance(trend_baseline)
-	print(trades[trades.all_trades == 1])
-	print(trades[trades.all_trades == -1])
-	print(trades[trades.all_trades == 'X'])
+	trades = strategy.zscore_distance(trend_baseline)
+	portfolio = apply_trades(prices, trades)
 
 if __name__ == "__main__":
 	main()
