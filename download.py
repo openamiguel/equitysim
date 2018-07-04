e8774766062075ce7b78596e148de1cc40125faf
openamiguel <openamiguel@gmail.com>
2018-07-03 21:59:49 -0700

    download.py version 2.0.1
    adds command prompts from prelim_download.py
    changed all logger debugs to logger infos

--- download.py
+++ download.py
@@ -1,8 +1,9 @@
 ## This code contains the re-consolidated download functions, and can perform any one of the following tasks:
 ## Download one stock (one-stock-one-file) from API, load one stock (one-stock-one-variable) from local drive, download many stocks (one-stock-one-file) from API, or load many stocks (many-stocks-one-variable) from local drive
 ## Author: Miguel Opeña
-## Version: 1.4.0
+## Version: 2.0.1
 
+import logging
 import pandas as pd
 import time
 import sys
@@ -31,7 +32,7 @@ def load_single(symbol, api_key, function="DAILY", interval="", output_size="ful
 	if function == "INTRADAY":
 		read_path = read_path + "&interval=" + interval
 	# Gives a tidbit of verbose output
-	logger.debug("Downloading " + symbol + " from AlphaVantage...")
+	logger.info("Downloading " + symbol + " from AlphaVantage...")
 	tick_data = None
 	# Accounts for the fact that AlphaVantage lacks certain high-volume ETFs and mutual funds
 	try:
@@ -39,17 +40,17 @@ def load_single(symbol, api_key, function="DAILY", interval="", output_size="ful
 	except ValueError:
 		logger.error(symbol + " not found by AlphaVantage. Download unsuccessful.")
 		return tick_data
-	logger.debug(symbol + " successfully downloaded!")
+	logger.info(symbol + " successfully downloaded!")
 	# Flips the data around (AlphaVantage presents it in reverse chronological order, but I prefer regular chronological)
 	tick_data = tick_data.reindex(index=tick_data.index[::-1])
 	# Saves ticker data to CSV, if requested
 	if writefile:
-		logger.debug("Saving data on " + symbol + "...")
+		logger.info("Saving data on " + symbol + "...")
 		write_path = folderpath + "/" + symbol + "_" + function
 		if interval != "": 
 			write_path = write_path + "&" + interval
 		tick_data.to_csv(write_path + "." + datatype)
-		logger.debug("Data on " + symbol + " successfully saved!")
+		logger.info("Data on " + symbol + " successfully saved!")
 	# Returns the data on symbol
 	return tick_data
 
@@ -64,15 +65,16 @@ def load_single_drive(symbol, function="DAILY", interval="", folderpath="", data
 	if interval != "":
 		readpath = readpath + "&" + interval
 	readpath = readpath + "." + datatype
-	logger.debug("Retrieving " + symbol + " from local drive...")
+	logger.info("Retrieving " + symbol + " from local drive...")
 	tick_data = None
 	try:
 		tick_data = pd.read_csv(readpath, index_col='timestamp')
 	except FileNotFoundError:
 		logger.error("Retrieval unsuccessful. File not found at " + readpath)
 		return tick_data
+	# De-duplicates the index
 	tick_data = tick_data[~tick_data.index.duplicated(keep='first')]
-	logger.debug("Data on " + symbol + " successfully retrieved!")
+	logger.info("Data on " + symbol + " successfully retrieved!")
 	return tick_data
 
 def load_separate(tickerverse, api_key, function="DAILY", interval="", output_size="full", folderpath="", datatype="csv"):
@@ -103,3 +105,36 @@ def load_combined_drive(tickerverse, column_choice="close", function="DAILY", in
 	# Makes each column the symbol of asset (to avoid confusion)
 	combined_output.columns = tickerverse
 	return combined_output
+
+def main():
+	""" User interacts with interface through command prompt, which obtains several "input" data. 
+		Here are some examples of how to run this program: 
+
+		python download.py -tickerUniverse SNP500 -folderPath C:/Users/Miguel/Documents/EQUITIES/stockDaily -apiKey <INSERT KEY> -function DAILY
+			This will download files of daily data on S&P 500 tickers to the desired folder path.
+
+		python download.py -tickerUniverse AAPL -folderPath C:/Users/Miguel/Documents/EQUITIES/stockIntraday1Min -apiKey <INSERT KEY> -function INTRADAY -interval 1min
+			This will download files of daily data on S&P 500 tickers to the desired folder path.
+
+		Inputs: implicit through command prompt
+		Outputs: 0 if everything works
+	"""
+	prompts = sys.argv
+	## Handles which symbol the user wants to download.
+	tickerverse, name = command_parser.get_tickerverse_from_prompts(prompts)
+	## Handles where the user wants to download their files. 
+	# Default folder path is relevant to the author only. 
+	folder_path = command_parser.get_generic_from_prompts(prompts, query="-folderPath", default="C:/Users/Miguel/Documents/EQUITIES/stockDaily", req=False)
+	## Handles the user's API key. 
+	api_key = command_parser.get_generic_from_prompts(prompts, query="-apiKey")
+	## Handles the desired time series function. 
+	function = command_parser.get_generic_from_prompts(prompts, query="-function")
+	## Handles the special case: if INTRADAY selected. 
+	interval = command_parser.get_generic_from_prompts(prompts, query="-interval") if function == "INTRADAY" else ""
+	## Handles user choice of separate or combined
+	load_separate(tickerverse, api_key, function=function, interval=interval, folderpath=folder_path)
+	## Closing output
+	logger.info("Download complete. Have a nice day!")
+
+if __name__ == "__main__":
+	main()
