3e6a0c47e00f4a035ec41ace07eb4fa5a4811185
openamiguel <openamiguel@gmail.com>
2018-07-06 14:17:18 -0700

    edgar_load.py version 1.3.0
    adds code to parse a submission file

--- edgar_load.py
+++ edgar_load.py
@@ -1,7 +1,7 @@
 ## This code gets company data from the SEC's Financial Statement Datasets.
 ## Link: https://www.sec.gov/dera/data/financial-statement-data-sets.html
 ## Author: Miguel Opeña
-## Version: 1.2.0
+## Version: 1.3.0
 
 import logging
 import os
@@ -58,17 +58,27 @@ def download_unzip(folderpath, startyear=2009, endyear=2018):
     return
 
 def get_sic_names(target_url='https://www.sec.gov/info/edgar/siccodes.htm'):
+    # Processes the raw data into dataframe
     webpage = pd.read_html(target_url)
     table = webpage[2]
+    # Column 0 has codes, Column 3 has industry titles
     sic_names = pd.concat([table[0], table[3]], axis=1)
     sic_names.dropna(how='any', axis=0, inplace=True)
     sic_names.drop(2, inplace=True)
     sic_names.columns = ['sic', 'industry_name']
+    # Cleans up the numeric column into integers
+    sic_names['sic'] = sic_names['sic'].apply(lambda x: int(x))
+    # Cleans up the format of dashes in entries
+    sic_names['industry_name'] = sic_names['industry_name'].apply(lambda x: x.replace('-', ' - ') if ' - ' not in x else x)
+    # Applies regular case conventions
     sic_names['industry_name'] = sic_names['industry_name'].apply(lambda_reg_case)
-    # Column 0 has codes, Column 3 has industry titles
+    return sic_names
 
-def submission_parse(filepath):
-    base_link = 'http://www.sec.gov/Archives/edgar/data/'
+def submission_parse(filepath, outpath):
+    """ Parses the data on submission files ONLY.
+        Inputs: path of submission file, output path of processed file
+        Outputs: True if everything works
+    """
     sub_df = pd.read_csv(filepath, sep='\t')
     # Does the easy processing: columns to keep as-is or simply rename
     proc_cols = ['adsh', 'cik', 'name', 'sic', 'former', 'changed', 'wksi', 
@@ -79,33 +89,64 @@ def submission_parse(filepath):
                 'changed': 'date_namechange', 
                 'wksi': 'well_known_seasoned_issuer'}
     sub_proc_df = sub_proc_df.rename(columns=col_dict)
+    # Cleans up the numeric columns into integers
+    sub_proc_df['central_index_key'] = sub_proc_df['central_index_key'].apply(lambda x: int(x))
+    sub_proc_df['sic'].fillna(-999, inplace=True)
+    sub_proc_df['sic'] = sub_proc_df['sic'].apply(lambda x: int(x))
     # "Processes" data source into the dataframe
     sub_proc_df['data_source'] = 'SEC_EDGAR'
     # Transforms company_name and former_name column using regular case
     sub_proc_df['company_name'] = sub_proc_df['company_name'].apply(lambda_reg_case)
     sub_proc_df['former_name'].fillna("", inplace=True)
     sub_proc_df['former_name'] = sub_proc_df['former_name'].apply(lambda_reg_case)
+    # Deletes any instance of suffix
+    lambda_remove_suffix = lambda x: x.split('/')[0].strip() if '/' in x else x
+    sub_proc_df['company_name'] = sub_proc_df['company_name'].apply(lambda_remove_suffix)
+    sub_proc_df['former_name'] = sub_proc_df['former_name'].apply(lambda_remove_suffix)
     # Makes date_namechange a nice date-related format
     sub_proc_df['date_namechange'].fillna("", inplace=True)
     sub_proc_df['date_namechange'] = sub_proc_df['date_namechange'].apply(lambda x: str(int(x))[:4] + "-" + str(int(x))[4:6] + "-" + str(int(x))[-2:] if x != '' else x)
-    ## TODO:
     # Changes sic into industry_title using SEC SIC codes
+    sic_names = get_sic_names()
+    sub_proc_df = sub_proc_df.merge(sic_names, how='left')
+    sub_proc_df.drop(labels='sic', axis=1, inplace=True)
+    # Builds source column (link to actual XML file)
+    # print(sub_proc_df.date_namechange)
+    base_link = 'http://www.sec.gov/Archives/edgar/data/'
+    sub_proc_df['source'] = base_link + sub_proc_df['central_index_key'].astype(str) + '/' + sub_proc_df['accession_num'].apply(lambda x: x.replace('-','')) + '/' + sub_proc_df['instance']
     # Transforms instance into ticker symbol, renamed to symbol
-    # Builds source column based on the following SQL query:
-    # select name,form,period, 'http://www.sec.gov/Archives/edgar/data/' 
-        # + ltrim(str(cik,10))+'/' + replace(adsh,'-','')+'/'+instance as url 
-        # from SUBM subm order by period desc, name
-    print(sub_proc_df.date_namechange)
+    lambda_to_symbol = lambda x: x.split('-')[0].upper() if '-' in x else x
+    sub_proc_df['instance'] = sub_proc_df['instance'].apply(lambda_to_symbol)
+    sub_proc_df = sub_proc_df.rename(columns={'instance':'symbol'})
+    # Deletes rows where symbol is not given (useless for my purposes)
+    sub_proc_df = sub_proc_df[pd.notnull(sub_proc_df['symbol'])]
+    # Writes to file
+    sub_proc_df.to_csv(outpath, sep='\t', index=False)
+    return True
+
+def number_parse(filepath, outpath):
     return
 
-def all_parse(folderpath):
+def presentation_parse(filepath, outpath):
+    return
+
+def tag_parse(filepath, outpath):
+    return
+
+def parse_in_directory(folderpath):
     for cur_path, directories, files in os.walk(folderpath, topdown=True):
         for file in files:
             path = os.path.join(cur_path,file)
             if "sub" in file:
-                submission_parse(path)
+                submission_parse(path, folderpath + "SUB_FINAL.txt")
+            elif "num" in file:
+                number_parse(path, folderpath + "NUM_FINAL.txt")
+            elif "pre" in file:
+                presentation_parse(path, folderpath + "PRE_FINAL.txt")
+            elif "tag" in file:
+                tag_parse(path, folderpath + "TAG_FINAL.txt")
     return
 
 folder_path = "C:/Users/Miguel/Desktop/EDGAR/"
 # download_unzip(folder_path, startyear=2018)
-all_parse(folder_path)
+parse_in_directory(folder_path)
