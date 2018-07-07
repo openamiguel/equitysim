a3ae1caf258b7f36d75e86a7d5353c1139788986
openamiguel <openamiguel@gmail.com>
2018-07-06 22:37:36 -0700

    edgar_load.py version 1.7.4
    added post-processing function to reduce data down to relevant ones (i.e. those in my stock data folder)
    still a work-in-progress

--- edgar_load.py
+++ edgar_load.py
@@ -1,8 +1,9 @@
 ## This code gets company data from the SEC's Financial Statement Datasets.
 ## Link: https://www.sec.gov/dera/data/financial-statement-data-sets.html
 ## Author: Miguel Opeña
-## Version: 1.6.3
+## Version: 1.7.4
 
+import csv
 import logging
 import os
 import pandas as pd
@@ -16,6 +17,10 @@ logger = logging.getLogger(__name__)
 
 lambda_reg_case = lambda x: " ".join([w.lower().capitalize() for w in x.split(" ")])
 
+writeheader = [True, True, True, True]
+csize = 100000
+
+# Move to separate file?
 def memory_check(filepath, threshold_ratio=10):
     """ Checks if file occupies too much RAM on the computer. 
         Inputs: file path to check, threshold ratio
@@ -26,16 +31,17 @@ def memory_check(filepath, threshold_ratio=10):
     # ram[1] gets the available RAM
     if filesize > threshold_ratio * ram[1] / 100.0:
         pct = filesize * 100 / ram[1]
-        logger.warn("File occupies {}%% of RAM.".format(pct))
+        logger.warning("File occupies %.2f percent of RAM.", pct)
     return
 
-def write_as_append(dataframe, filepath):
+# Move to separate file?
+def write_as_append(dataframe, filepath, index=False, header=False, sep='\t'):
     """ Writes dataframe to file in append mode. 
         Inputs: dataframe, file path to append to
         Outputs: True if everything works
     """
     with open(filepath, 'a') as outfile:
-        dataframe.to_csv(outfile, sep='\t', index=False)
+        dataframe.to_csv(outfile, sep=sep, header=header)
     outfile.close()
     return True
 
@@ -97,15 +103,15 @@ def submission_parse(filepath, outpath):
         Inputs: path of submission file, output path of processed file
         Outputs: True if everything works
     """
-    sub_df = pd.read_csv(filepath, sep='\t')
+    sub_df = pd.read_csv(filepath, sep='\t', encoding='iso8859-1', quoting=csv.QUOTE_NONE)
     # Does the easy processing: columns to keep as-is or simply rename
     proc_cols = ['adsh', 'cik', 'name', 'sic', 'former', 'changed', 'wksi', 
                  'form', 'detail', 'instance', 'nciks', 'aciks']
     sub_proc_df = pd.concat([sub_df[col] for col in proc_cols], axis=1)
     col_dict = {'adsh': 'accession_num', 'cik': 'central_index_key', 
                 'name': 'company_name', 'former': 'former_name', 
-                'changed': 'date_namechange', 
-                'wksi': 'well_known_seasoned_issuer'}
+                'changed': 'date_of_name_change', 
+                'wksi': 'is_well_known_seasoned_issuer'}
     sub_proc_df = sub_proc_df.rename(columns=col_dict)
     # Cleans up the numeric columns into integers
     sub_proc_df['central_index_key'] = sub_proc_df['central_index_key'].apply(lambda x: int(x))
@@ -121,15 +127,15 @@ def submission_parse(filepath, outpath):
     lambda_remove_suffix = lambda x: x.split('/')[0].strip() if '/' in x else x
     sub_proc_df['company_name'] = sub_proc_df['company_name'].apply(lambda_remove_suffix)
     sub_proc_df['former_name'] = sub_proc_df['former_name'].apply(lambda_remove_suffix)
-    # Makes date_namechange a nice date-related format
-    sub_proc_df['date_namechange'].fillna("", inplace=True)
-    sub_proc_df['date_namechange'] = sub_proc_df['date_namechange'].apply(lambda x: str(int(x))[:4] + "-" + str(int(x))[4:6] + "-" + str(int(x))[-2:] if x != '' else x)
-    # Changes sic into industry_title using SEC SIC codes
+    # Makes date_of_name_change a nice date-related format
+    sub_proc_df['date_of_name_change'].fillna("", inplace=True)
+    sub_proc_df['date_of_name_change'] = sub_proc_df['date_of_name_change'].apply(lambda x: str(int(x))[:4] + "-" + str(int(x))[4:6] + "-" + str(int(x))[-2:] if x != '' else x)
+    # Changes sic into industry_name using SEC SIC codes
     sic_names = get_sic_names()
     sub_proc_df = sub_proc_df.merge(sic_names, how='left')
     sub_proc_df.drop(labels='sic', axis=1, inplace=True)
     # Builds source column (link to actual XML file)
-    # print(sub_proc_df.date_namechange)
+    # print(sub_proc_df.date_of_name_change)
     base_link = 'http://www.sec.gov/Archives/edgar/data/'
     sub_proc_df['source'] = base_link + sub_proc_df['central_index_key'].astype(str) + '/' + sub_proc_df['accession_num'].apply(lambda x: x.replace('-','')) + '/' + sub_proc_df['instance']
     # Transforms instance into ticker symbol, renamed to symbol
@@ -139,7 +145,8 @@ def submission_parse(filepath, outpath):
     # Deletes rows where symbol is not given (useless for my purposes)
     sub_proc_df = sub_proc_df[pd.notnull(sub_proc_df['symbol'])]
     # Writes to file
-    write_as_append(sub_proc_df, outpath)
+    write_as_append(sub_proc_df, outpath, header=writeheader[0])
+    writeheader[0] = False
     return True
 
 def number_parse(filepath, outpath):
@@ -147,7 +154,7 @@ def number_parse(filepath, outpath):
         Inputs: path of number file, output path of processed file
         Outputs: True if everything works
     """
-    num_df = pd.read_csv(filepath, sep='\t')
+    num_df = pd.read_csv(filepath, sep='\t', encoding='iso8859-1', quoting=csv.QUOTE_NONE)
     # Does the easy processing: columns to keep as-is or simply rename
     proc_cols = ['adsh', 'tag', 'version', 'ddate', 'qtrs', 'uom', 'value']
     num_proc_df = pd.concat([num_df[col] for col in proc_cols], axis=1)
@@ -156,7 +163,8 @@ def number_parse(filepath, outpath):
                 'qtrs': 'num_quarters'}
     num_proc_df = num_proc_df.rename(columns=col_dict)
     # Writes to file
-    write_as_append(num_proc_df, outpath)
+    write_as_append(num_proc_df, outpath, header=writeheader[1])
+    writeheader[1] = False
     return True
 
 def presentation_parse(filepath, outpath):
@@ -164,7 +172,7 @@ def presentation_parse(filepath, outpath):
         Inputs: path of presentation file, output path of processed file
         Outputs: True if everything works
     """
-    pre_df = pd.read_csv(filepath, sep='\t')
+    pre_df = pd.read_csv(filepath, sep='\t', encoding='iso8859-1', quoting=csv.QUOTE_NONE)
     # Does the easy processing: columns to keep as-is or simply rename
     proc_cols = ['adsh', 'report', 'stmt', 'line', 'tag', 'version']
     pre_proc_df = pd.concat([pre_df[col] for col in proc_cols], axis=1)
@@ -174,7 +182,8 @@ def presentation_parse(filepath, outpath):
     # Combines statement and report data into new column and deletes the old one
     pre_proc_df['statement_report'] = pre_proc_df['stmt'] + "_" + pre_proc_df['report'].astype(str)
     pre_proc_df.drop(labels=['stmt', 'report'], axis=1, inplace=True)
-    write_as_append(pre_proc_df, outpath)
+    write_as_append(pre_proc_df, outpath, header=writeheader[2])
+    writeheader[2] = False
     return True
 
 def tag_parse(filepath, outpath):
@@ -182,7 +191,7 @@ def tag_parse(filepath, outpath):
         Inputs: path of tag file, output path of processed file
         Outputs: True if everything works
     """
-    tag_df = pd.read_csv(filepath, sep='\t')
+    tag_df = pd.read_csv(filepath, sep='\t', encoding='iso8859-1', quoting=csv.QUOTE_NONE)
     # Does the easy processing: columns to keep as-is or simply rename
     proc_cols = ['tag', 'version', 'abstract', 'datatype', 
                  'iord', 'crdr', 'doc']
@@ -194,25 +203,129 @@ def tag_parse(filepath, outpath):
     lambda_swap = lambda x: int(not x)
     tag_proc_df['abstract'] = tag_proc_df['abstract'].apply(lambda_swap)
     tag_proc_df = tag_proc_df.rename(columns={'abstract':'is_numeric'})
-    write_as_append(tag_proc_df, outpath)
+    write_as_append(tag_proc_df, outpath, header=writeheader[3])
+    writeheader[3] = False
     return True
 
 def parse_in_directory(folderpath):
+    """ Parses the data on all files in a directory.
+        Inputs: path of target directory
+        Outputs: True if everything works
+    """
     for cur_path, directories, files in os.walk(folderpath, topdown=True):
         for file in files:
             path = os.path.join(cur_path,file)
             memory_check(path)
             logger.debug("Processing {}".format(path))
             if "sub" in file:
-                submission_parse(path, folderpath + "SUB_FINAL.txt")
+                submission_parse(path, folderpath + "SUB_INTERMEDIATE.txt")
             elif "num" in file:
-                number_parse(path, folderpath + "NUM_FINAL.txt")
+                number_parse(path, folderpath + "NUM_INTERMEDIATE.txt")
             elif "pre" in file:
-                presentation_parse(path, folderpath + "PRE_FINAL.txt")
+                presentation_parse(path, folderpath + "PRE_INTERMEDIATE.txt")
             elif "tag" in file:
-                tag_parse(path, folderpath + "TAG_FINAL.txt")
-    return
+                tag_parse(path, folderpath + "TAG_INTERMEDIATE.txt")
+    return True
+
+# Move to separate file?
+def get_current_symbols(folderpath, function="DAILY", datatype="csv"):
+    """ Returns list of all symbols downloaded to given folder. 
+        Inputs: path of folder directory, time series function, data type
+        Outputs: list of aforementioned
+    """
+    symbols = []
+    # Walks through the given folderpath
+    for cur_path, directories, files in os.walk(folderpath):
+        for file in files:
+            # Skips all irrelevant files
+            if datatype not in file or function not in file:
+                continue
+            # Process the file name for symbol
+            symbols.append(file.split('_')[0])
+    return symbols
+            
+
+def post_proc(folderpath, stock_folderpath, outfolder):
+    """ Post-processing on data of interest.
+        Inputs: path of tag file, output path of processed file
+        Outputs: True if everything works
+    """
+    ## Loads all stock data downloaded to given folder
+    symbols = get_current_symbols(stock_folderpath)
+    #########################################################
+    ## Loads SUB_INTERMEDIATE from its location            ##
+    #########################################################
+    logger.debug("Reading %s file from local drive...", "SUB")
+    sub_inter_df = pd.read_csv(folderpath + "SUB_INTERMEDIATE.txt", sep='\t', encoding='iso8859-1')
+    # Drop non-unique CIK values
+    sub_inter_df.drop_duplicates(subset=['central_index_key'], inplace=True)
+    # Drop rows with symbols not in the symbol list
+    sub_inter_df = sub_inter_df[sub_inter_df.symbol.isin(symbols)]
+    # Gets all the columns of interest for json metadata
+    json_meta = pd.concat([sub_inter_df.symbol, sub_inter_df.company_name, 
+                           sub_inter_df.central_index_key, sub_inter_df.industry_name, 
+                           sub_inter_df.former_name, sub_inter_df.date_of_name_change, 
+                           sub_inter_df.is_well_known_seasoned_issuer, sub_inter_df.data_source], axis=1)
+    # Saves the accession number column for later use
+    accession_list = list(sub_inter_df.accession_num.values)
+    # Frees up some memory and writes SUB FINAL to file
+    sub_inter_df = sub_inter_df.to_csv(outfolder + "SUB_FINAL.txt", sep='\t', index=False)
+    #########################################################
+    ## Loads TAG_INTERMEDIATE from its location            ##
+    #########################################################
+    logger.debug("Reading %s file from local drive...", "TAG")
+    tag_inter_df = pd.read_csv(folderpath + "TAG_INTERMEDIATE.txt", sep='\t', encoding='iso8859-1')
+    # Drops non-unique values based on tag name AND tag version
+    tag_inter_df.drop_duplicates(subset=['tag_name', 'tag_version'], inplace=True)
+    # Frees up memory and writes TAG FINAL to file
+    tag_inter_df = tag_inter_df.to_csv(outfolder + "TAG_FINAL.txt", sep='\t', index=False)
+    #########################################################
+    ## Loads NUM_INTERMEDIATE from its location in chunks  ##
+    #########################################################
+    logger.debug("Reading %s file from local drive...", "NUM")
+    num_chunks = pd.read_csv(folderpath + "NUM_INTERMEDIATE.txt", sep='\t', chunksize=csize, encoding='iso8859-1')
+    writeHeader = True
+    with open(outfolder + "NUM_FINAL.txt", 'w') as numfinalfile:
+        for chunk in num_chunks:
+            logger.debug("Accession #%s being read...", chunk.accession_num[chunk.index[0]])
+            chunk_filter = chunk[chunk.accession_num.isin(accession_list)]
+            chunk_filter.to_csv(numfinalfile, sep='\t', index=False, header=writeHeader)
+            writeHeader = False
+    numfinalfile.close()
+    #########################################################
+    ## Loads PRE_INTERMEDIATE from its location in chunks  ##
+    #########################################################
+    logger.debug("Reading %s file from local drive...", "PRE")
+    pre_chunks = pd.read_csv(folderpath + "PRE_INTERMEDIATE.txt", sep='\t', chunksize=csize, encoding='iso8859-1')
+    writeHeader = True
+    with open(outfolder + "PRE_FINAL.txt", 'w') as prefinalfile:
+        for chunk in pre_chunks:
+            logger.debug("Accession #%s being read...", chunk.accession_num[chunk.index[0]])
+            chunk_filter = chunk[chunk.accession_num.isin(accession_list)]
+            chunk_filter.to_csv(prefinalfile, sep='\t', index=False, header=writeHeader)
+            writeHeader = False
+    prefinalfile.close()
+    # For each symbol in json_meta, processes into a different JSON file
+    for ind in json_meta.index:
+        this_symbol = json_meta.symbol[ind]
+        logger.debug("Processing %s financal statements...", this_symbol)
+        filename = outfolder + "{}_Financials.json".format(this_symbol)
+        json_meta_one = json_meta.loc[[ind]].to_json(orient='records')
+        # Indicates that this info is metadata
+        json_meta_one = json_meta_one[:1] + "\"metadata\":" + json_meta_one[1:]
+        json_meta_one = json_meta_one.replace('[', '')
+        json_meta_one = json_meta_one.replace(']', '')
+        json_meta_one = "{" + json_meta_one + ",\n"
+        with open(filename, 'a') as jsonfile:
+            jsonfile.write(json_meta_one)
+        break
+    ## TODO:
+    # Walk through NUM (in chunks) and drop rows with accession_num not in SUB
+    # Walk through PRE (in chunks) and drow rows with accession_num not in SUB
 
 folder_path = "C:/Users/Miguel/Desktop/EDGAR/"
-# download_unzip(folder_path, startyear=2017)
-parse_in_directory(folder_path)
+stock_folder_path = "C:/Users/Miguel/Documents/EQUITIES/stockDaily"
+outpath = "C:/Users/Miguel/Desktop/"
+# download_unzip(folder_path, endyear=2016)
+# parse_in_directory(folder_path)
+post_proc(folder_path, stock_folder_path, outpath)
