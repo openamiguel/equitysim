521bea483bc017e24fdfeb7d19d1c78bee83ffcd
openamiguel <openamiguel@gmail.com>
2018-07-15 21:39:18 -0700

    metadata_add.py version 1.0.5
    sprinkled some comments on the code

--- metadata_add.py
+++ metadata_add.py
@@ -1,6 +1,6 @@
 ## General program to add metadata to the Financials JSON files.
 ## Author: Miguel Opeña
-## Version: 1.0.4
+## Version: 1.0.5
 
 from datetime import datetime
 import logging
@@ -12,14 +12,18 @@ logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 def date_transform(date, delim='/'):
-	""" Transforms a date string into another format.
-        Inputs: date string, desired format (default: AlphaVantage format "YYYY-MM-DD")
+	""" Transforms a date string into another format. Warning: currently hardcoded.
+        Inputs: date string, delimiter for date string
         Outputs: reformatted date
 	"""
 	date_split = date.split(delim)
 	return '-'.join([date_split[2], date_split[0], date_split[1]])
 
 def stock_split(folderpath, link="http://eoddata.com/splits.aspx", datasource="EOD_Data"):
+	""" Saves stock split data from EOD Data to a row for the Financials JSON files.
+        Inputs: folder path to look for currently-downloaded (daily) symbols; link to data source, name thereof
+        Outputs: table of relevant stock split data
+	"""
 	# Reads data from link
 	data = pd.read_html(link)
 	# Hardcoded for the default link (no other choice?)
